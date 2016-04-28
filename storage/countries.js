270d7c3b00bcc5252e780c9b44edbfa33a1967d0
zer0latency <dandydan2k@gmail.com>
2016-04-28 17:57:10 +0300

    Indent fix

--- storage/countries.js
+++ storage/countries.js
@@ -1,7 +1,7 @@
 module.exports = {
     fetch: function () {
         return {
-            "russia":  { "text": "Россия",  "code": "+7",  "sprite_offset": "0"  },
+            "russia":  { "text": "Россия",  "code": "+7",  "sprite_offset":  "0"  },
             "ukraine": { "text": "Украина", "code": "+38", "sprite_offset": "-17" },
             "iran":    { "text": "Иран",    "code": "+98", "sprite_offset": "-34" }
         };
